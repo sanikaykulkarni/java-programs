3381ecbd2f789627a19b1d361a84c35a704a8b51
sanikakulkarni <sanikakulkarni28@gmail.com>
2023-06-09 10:35:00 -0700

    four programs are added

---
+++ prime.java
@@ -0,0 +1,30 @@
+public class Prime 
+{    
+ public static void main(String args[]){ 
+ Scanner input=new Scanner(System.in);  
+ int i,m=0,flag=0,n;           
+ System.out.println("Enter number to check prime or not ");
+ n = input.nextInt();   
+ m=n/2;          
+ if(n==0||n==1)    
+ {  
+  System.out.println(n+" is not prime number");         
+ }    
+ else    
+ {      
+  for(i=2;i<=m;i++)   
+  {      
+   if(n%i==0)  
+   {      
+    System.out.println(n+" is not prime number");               
+    flag=1;                 
+    break;   
+   }          
+  }          
+  if(flag==0)  
+  { 
+   System.out.println(n+" is prime number");  
+  }      
+ }//end of else     
+ }   
+}
